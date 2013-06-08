086b021ce10e160239b52b4dba5229323adfeb34
Robert Zhang <louirobert@gmail.com>
2013-06-08 10:06:41 +0800

    Speed up Cutting Sticks by easy section length computing.

--- ch11_ex5.cpp
+++ ch11_ex5.cpp
@@ -4,23 +4,18 @@
 
 using namespace std;
 
-inline int len(const vector<int> & sec, int i, int j) {
-  int r = 0;
-  for (; i <= j; i++)
-    r += sec[i];
-  return r;
-}
-
 int cut(int l, const vector<int> & pos) {
   if (pos.size() < 1)
     return pos.empty() ? 0 : l;
-  vector<int> sec(pos.size() + 1);
-  sec[0] = pos[0];
-  sec[sec.size() - 1] = l - pos.back();
-  for (int i = 1; i < pos.size(); i++) {
-    sec[i] = pos[i] - pos[i - 1];
-  }
-  int n = sec.size();
+  //n cut positions divides one stick into n + 1 sections.
+  //sec stores the beginning position of a section. To make it easy to calculate
+  //a section length, sec stores the length of the stick as the begnning of a section
+  //beyond the n + 1 real sections.
+  vector<int> sec;
+  sec.push_back(0);
+  sec.insert(sec.end(), pos.begin(), pos.end());
+  sec.push_back(l);
+  int n = sec.size() - 1;
   vector<vector<int> > seg(n, vector<int>(n, 0));
   for (int j = 1; j < n; j++) {
     for (int i = j - 1; i >= 0; i--) {
@@ -31,7 +26,7 @@ int cut(int l, const vector<int> & pos) {
         if (s < m)
           m = s;
       }
-      seg[i][j] = m + len(sec, i, j);
+      seg[i][j] = m + (sec[j + 1] - sec[i]); //m + the length of sections from i to j
     }
   }
   return seg[0][n - 1];
