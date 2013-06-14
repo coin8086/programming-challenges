41704b6e8724b0dca68511b89059200d4b042943
Robert Zhang <louirobert@gmail.com>
2013-06-14 15:30:50 +0800

    Add Primary Arithmetic.

---
+++ ch5_ex1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+#define MAX_SIZE 10
+
+using namespace std;
+
+inline vector<char> to_vec(int n) {
+  vector<char> v;
+  v.reserve(MAX_SIZE);
+  while (n) {
+    v.push_back(n % 10);
+    n /= 10;
+  }
+  return v;
+}
+
+int carry_times(int n, int m) {
+  if (!n || !m)
+    return 0;
+  vector<char> bn = to_vec(n);
+  vector<char> bm = to_vec(m);
+  int carry = 0;
+  int times = 0;
+  int size = n >= m ? bn.size() : bm.size();
+  bn.resize(size);
+  bm.resize(size);
+  for (int i = 0; i < size; i++) {
+    int s = carry + bn[i] + bm[i];
+    if (s > 9) {
+      carry = 1;
+      times++;
+    }
+    else {
+      carry = 0;
+    }
+  }
+  return times;
+}
+
+int main() {
+  int n, m;
+  while ((cin >> n >> m) && (n || m)) {
+    int t = carry_times(n, m);
+    if (!t)
+      cout << "No carry operation.";
+    else if (t == 1)
+      cout << "1 carry operation.";
+    else
+      cout << t << " carry operations.";
+    cout << endl;
+  }
+  return 0;
+}
---
+++ ch5_ex1_2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#define MAX_SIZE 10
+
+using namespace std;
+
+int carry_times(vector<char> & n, vector<char> & m) {
+  int carry = 0;
+  int times = 0;
+  int size = n.size() >= m.size() ? n.size() : m.size();
+  n.resize(size);
+  m.resize(size);
+  for (int i = 0; i < size; i++) {
+    int s = carry + n[i] + m[i];
+    if (s > 9) {
+      carry = 1;
+      times++;
+    }
+    else {
+      carry = 0;
+    }
+  }
+  return times;
+}
+
+inline vector<char> read_n(istream & is) {
+  vector<char> n;
+  n.reserve(MAX_SIZE);
+  char ch;
+  while (is.get(ch) && ch != ' ')
+    n.push_back(ch - '0');
+  reverse(n.begin(), n.end());
+  return n;
+}
+
+int main() {
+  string line;
+  while (getline(cin, line)) {
+    if (line == "0 0")
+      break;
+    istringstream is(line);
+    vector<char> n = read_n(is);
+    vector<char> m = read_n(is);
+    int t = carry_times(n, m);
+    if (!t)
+      cout << "No carry operation.";
+    else if (t == 1)
+      cout << "1 carry operation.";
+    else
+      cout << t << " carry operations.";
+    cout << endl;
+  }
+  return 0;
+}
