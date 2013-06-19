a195f66e7c1b579c1e5c08aff9369a3c11faf7e1
Robert Zhang <louirobert@gmail.com>
2013-06-19 18:18:18 +0800

    Add Counting.

---
+++ ch6_ex3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include "bigint.h"
+
+using namespace std;
+
+vector<BigInt> N;
+
+void init() {
+  N.reserve(1001);
+  N.push_back(0);
+  N.push_back(2);
+  N.push_back(5);
+  N.push_back(13);
+}
+
+BigInt count(int n) {
+  if (n >= N.size()) {
+    for (int i = N.size(); i <= n; i++) {
+      BigInt bi = 2 * N[i - 1] + N[i - 2] + N[i - 3];
+      N.push_back(bi);
+    }
+  }
+  return N[n];
+}
+
+int main() {
+  init();
+  int n;
+  while (cin >> n) {
+    cout << count(n) << endl;
+  }
+  return 0;
+}
+
